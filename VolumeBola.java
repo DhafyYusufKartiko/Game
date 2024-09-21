597d1f55be5ae0209e3f38549d552a30cdb4c01e
DhafyYusufKartiko <dhafyyusuf@gmail.com>
2024-09-21 13:42:18 +0700

    Add files via upload

---
+++ VolumeBola.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author USER
+ */
+public class VolumeBola {
+
+    public static void main(String[] args) {
+        double jariJari = 9;
+
+        double pi = 3.141592;
+        double volume = 4 / 3d * pi * jariJari * jariJari;
+        System.out.println("Volume bola : " + volume);
+
+    }
+
+}
